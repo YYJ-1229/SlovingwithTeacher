fc8299de43d7a4b21a794857c2d91ee77aca7d8f
YYJ-1229 <90436@naver.com>
2022-12-15 01:52:45 +0900

    [feature]질문게시판 CRUD 구현

---
+++ src/main/java/com/SolvingWithTeacher/demo/web/dto/PostResponseDto.java
@@ -0,0 +1,21 @@
+package com.SolvingWithTeacher.demo.web.dto;
+
+
+import com.SolvingWithTeacher.demo.domain.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
